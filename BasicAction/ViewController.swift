b9067eb22806f2d41230fd0b2b2770c06b606604
Saleh Albatati <0xack13@gmail.com>
2016-01-29 20:57:52 +0300

    Initial Commit

---
+++ BasicAction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicAction
+//
+//  Created by 0xack13 on 1/29/16.
+//  Copyright © 2016 0xack13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
